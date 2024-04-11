e727dcff509c5ade181e6b65fb0c2d778feb08e9
ppalladio <nine.slide@gmail.com>
2024-04-11 21:50:00 +0200

    init for extranjeria

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 require('dotenv').config();
+const { timeout } = require('puppeteer');
 const pp = require('puppeteer');
-
 // *-----*
+const APPOINTMENT_PAGE =
+    'https://icp.administracionelectronica.gob.es/icpplustieb/citar?p=8&locale=es';
 
+console.log(process.env.USERNAME);
 // *-----*
 const specialty = 'Dermatología';
 const appointmentType = '';
@@ -16,103 +19,112 @@ const doctor = '';
  * @return void
  */
 async function Login(page) {
-    await page.goto(LOGIN_PAGE, {
-        timeout: 10000,
-    });
-    // acceptCookies
-    setTimeout(() => {}, 10000);
-    // input login details
-    try {
-        const usernameField = await page.waitForSelector('#ion-input-0');
-        await usernameField.type(USERNAME);
+    await page.goto(APPOINTMENT_PAGE, { waitUntil: 'networkidle0' });
+	const officesButton = await page.waitForSelector('div.fld>select#sede ');
+    await officesButton.click();
+    const officesAvailable = await page.$$('#sede > optgroup > option');
+    const extractedData = await Promise.all(
+        officesAvailable.map(async (office) => {
+            return await office.evaluate((el) => ({
+                id: el.value,
+                textContent: el.textContent,
+            }));
+        }),
+    );
+	await page.select('#sede',extractedData[0].id)
+	//@ get all the available appointments types for the selected office
 
-        const passwordField = await page.waitForSelector('#ion-input-1');
-        await passwordField.type(PASSWORD);
-
-        await page.click('.san-button', {
-            waitUntil: 'networkidle0',
-        });
-        // await page.keyboard.press('Esc');
-    } catch (error) {
-        console.log('credential error', error);
-        return error;
-    }
-    await page.goto(APPOINTMENT_PAGE, { timeout: 10000 });
+	const appointmentType = await page.select('.mf-input__l',20)
+	console.log(appointmentType)
     // await page.waitForNavigation({ waitUntil: 'networkidle0' });
     // console.log('login completed');
+
+    // await specialtySelector[0].click();
 }
 /**
  * A function to get available appointments based on specified criteria.
  *
  */
-async function GetAvailableAppointments(page) {
-    const specialtySelector = await page.$$(
-        ' div.msan-appointments-selection-v2__selectors-container > msan-selection-v2-specialties > san-select > article > ion-item > div.san-select__container > ionic-selectable > div.ionic-selectable-inner > button',
-    );
-    console.log(specialtySelector);
-    await specialtySelector[0].click();
+// async function GetAvailableAppointments(page) {
+//     await page.goto(APPOINTMENT_SELECTION_PAGE);
+// 	await page.evaluate(() => {
+// 		const allSpanElements = document.querySelectorAll('span');
+// 		for (const span of allSpanElements) {
+// 			if (span.textContent.includes('Pedie nueva cita')) {
+// 				// Perform actions on the span
+// 				span.click().then(() => console.log('clicked')).finally(); // Example action
+// 				break; // Stop if you only need the first match
+// 			}
+// 		}
+// 	});
+// console.log(button)
+//     const specialtySelector = await page.$$(
+//         ' san-select.msan-selection-v2-specialties__select > article > ion-item > div.san-select__container > ionic-selectable > div.ionic-selectable-inner > button',
+//     );
+//     console.log(specialtySelector);
+//     await specialtySelector[0].click();
 
-    const inputSpecialty = await page.$$(
-        'ion-searchbar>div.searchbar-input-container.sc-ion-searchbar-md>input',
-    );
-    console.log(inputSpecialty);
-    // const typeSpecialty = await inputSpecialty[0].type(specialty);
+//     // const inputSpecialty = await page.$$(
+//     //     'ion-searchbar>div.searchbar-input-container.sc-ion-searchbar-md>input',
+//     // );
+//     // console.log("inputSpecialty",inputSpecialty);
+//     // const typeSpecialty = await inputSpecialty[0].type(specialty);
 
-    // console.log(typeSpecialty);
-    const appointmentTypeSelector = await page.$$(
-        'div.msan-selection-v2-reasons > san-select-filter > div',
-    );
-    await appointmentTypeSelector[0].click();
-    console.log(appointmentTypeSelector);
+//     // console.log(typeSpecialty);
+//     // const appointmentTypeSelector = await page.$$(
+//     //     'div.msan-selection-v2-reasons > san-select-filter > div',
+//     // );
+//     // await appointmentTypeSelector[0].click();
+//     // console.log("appointmentTypeSelector",appointmentTypeSelector);
 
-    const inputAppointmentType = await page.$$(
-        'ion-header > ion-searchbar > div.searchbar-input-container.sc-ion-searchbar-md > input',
-    );
-    console.log(inputAppointmentType);
-    // get all the available appointments types
-    const typeSelector = await page.$$(
-        'section.san-select-filter-modal-checkbox__list > article',
-    );
-    for (let type of typeSelector) {
-        const test = type.$();
-    }
+//     // const inputAppointmentType = await page.$$(
+//     //     'ion-header > ion-searchbar > div.searchbar-input-container.sc-ion-searchbar-md > input',
+//     // );
+//     // console.log("inputAppointmentType",inputAppointmentType);
+//     // // get all the available appointments types
+//     // const typeSelector = await page.$$(
+//     //     'section.san-select-filter-modal-checkbox__list > article',
+//     // );
+//     // for (let type of typeSelector) {
+//     //     const test = type.$();
+//     // }
 
-    // const appointmentLocationSelector = await page.waitForSelector();
-    // const appointmentCenterSelector = await page.waitForSelector();
-    // const appointmentDoctorSelector = await page.waitForSelector();
-    // const asapButtonSelector = await page.waitForSelector();
-    // try {
-    //     await Promise.all([
-    //         specialtySelector.select(specialty), //!
-    //         appointmentTypeSelector.select(appointmentType),
-    //         appointmentLocationSelector.select(appointmentLocation),
-    //     ]);
-    // } catch (error) {
-    //     console.log(
-    //         'review your specialty, appointment type, appointment location',
-    //     );
-    // }
-    // if (medicalCenter || doctor) {
-    //     try {
-    //         await Promise.all([
-    //             appointmentCenterSelector.select(medicalCenter),
-    //             appointmentDoctorSelector.select(doctor),
-    //         ]);
-    //     } catch (error) {
-    //         console.log('please verify your medical center or doctor');
-    //     }
-    // }
+//     // const appointmentLocationSelector = await page.waitForSelector();
+//     // const appointmentCenterSelector = await page.waitForSelector();
+//     // const appointmentDoctorSelector = await page.waitForSelector();
+//     // const asapButtonSelector = await page.waitForSelector();
+//     // try {
+//     //     await Promise.all([
+//     //         specialtySelector.select(specialty), //!
+//     //         appointmentTypeSelector.select(appointmentType),
+//     //         appointmentLocationSelector.select(appointmentLocation),
+//     //     ]);
+//     // } catch (error) {
+//     //     console.log(
+//     //         'review your specialty, appointment type, appointment location',
+//     //     );
+//     // }
+//     // if (medicalCenter || doctor) {
+//     //     try {
+//     //         await Promise.all([
+//     //             appointmentCenterSelector.select(medicalCenter),
+//     //             appointmentDoctorSelector.select(doctor),
+//     //         ]);
+//     //     } catch (error) {
+//     //         console.log('please verify your medical center or doctor');
+//     //     }
+//     // }
 
-    // try {
-    //     await asapButtonSelector.click();
-    //     await page.waitForNavigation({ waitUntil: 'networkidle0' });
-    // } catch (error) {
-    //     console.log(
-    //         'cant click on Lo Antes Posible button, review specialty,type and province',
-    //     );
-    // }
-    // @ next page
-}
+//     // try {
+//     //     await asapButtonSelector.click();
+//     //     await page.waitForNavigation({ waitUntil: 'networkidle0' });
+//     // } catch (error) {
+//     //     console.log(
+//     //         'cant click on Lo Antes Posible button, review specialty,type and province',
+//     //     );
+//     // }
+//     // @ next page
+// }
 
 // main
 async function main() {
@@ -129,15 +141,15 @@ async function main() {
     //     await browser.close();
     //     return;
     // }
-    try {
-        const button = await page.waitForSelector(
-            'div.appointments-manager__button > san-button > article > ion-button',
-        );
-        await Promise.all([
-            button.click(),
-            page.waitForNavigation({ waitUntil: 'networkidle0' }),
-        ]);
-    } catch (error) {}
-    await GetAvailableAppointments(page);
+    // try {
+    //     const button = await page.waitForSelector(
+    //         'div.appointments-manager__button > san-button > article > ion-button',
+    //     );
+    //     await Promise.all([
+    //         button.click(),
+    //         page.waitForNavigation({ waitUntil: 'networkidle0' }),
+    //     ]);
+    // } catch (error) {}
+    // await GetAvailableAppointments(page);
 }
 main();
